404629d5d76e18382f9fade78da540310acfced2
Alektas <kriowolf1@gmail.com>
2020-03-24 20:07:42 +0300

    feature: device data graph

--- app/src/main/java/alektas/sensor/ui/device/DeviceFragment.kt
+++ app/src/main/java/alektas/sensor/ui/device/DeviceFragment.kt
@@ -5,6 +5,11 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 
 import android.view.*
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import com.jjoe64.graphview.series.DataPoint
+import com.jjoe64.graphview.series.LineGraphSeries
 import kotlinx.android.synthetic.main.fragment_device.*
 
 const val ARG_DEVICE_NAME = "device_name"
@@ -12,6 +17,8 @@ const val ARG_DEVICE_MAC = "device_mac"
 const val ARG_DEVICE_RSSI = "device_rssi"
 
 class DeviceFragment : Fragment() {
+    private val viewModel: DeviceViewModel by viewModels()
+    private var dataSeries = LineGraphSeries<DataPoint>()
     private var name: String? = null
     private var address: String? = null
     private var rssi: Int? = null
@@ -37,6 +44,28 @@ class DeviceFragment : Fragment() {
         device_name.text = name ?: getString(R.string.unknown_device_name)
         device_address.text = address
         device_rssi.text = rssi.toString()
+
+        dataSeries.color = ContextCompat.getColor(requireContext(), R.color.colorGraphLines)
+        device_data_graph.apply {
+            viewport.apply {
+                isXAxisBoundsManual = true
+                maxXAxisSize = 100.0
+                setMaxX(100.0)
+                isYAxisBoundsManual = true
+                setMaxY(128.0)
+                setMinY(-128.0)
+            }
+            addSeries(dataSeries)
+        }
+
+        viewModel.onViewCreated()
+        viewModel.cashedData.observe(viewLifecycleOwner, Observer {
+            dataSeries.resetData(it)
+        })
+        viewModel.data.observe(viewLifecycleOwner, Observer {
+            println(it)
+            dataSeries.appendData(it, true, 100)
+        })
     }
 
 }
